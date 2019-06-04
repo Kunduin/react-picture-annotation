d6e4af7cb26bb9504b5d770de1e1b4d70431d126
Kunduin <15852158126@163.com>
2019-06-04 23:38:26 +0800

    test(storybook): init storybook config

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = ({ config, mode }) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve("babel-loader"),
+        options: {
+          presets: [require.resolve("babel-preset-react-app")]
+        }
+      },
+      require.resolve("react-docgen-typescript-loader")
+    ]
+  });
+  config.resolve.extensions.push(".ts", ".tsx");
+  return config;
+};
